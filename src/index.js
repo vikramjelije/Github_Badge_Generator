300a15d455026e1520c7bac648afa903161781c8
vikramjelije <vikky1996.com@gmail.com>
2021-12-23 13:00:29 +0530

    Changed the favicon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.render(  
   <React.StrictMode>
     <App />
   </React.StrictMode>,
